0b7a0ad9e9f48acedec65078b03eab5a9051c34d
Kostas Dritsas <kostas@kostasmbp.local>
2025-07-14 20:23:48 +0300

    add show page and Memories context

--- lib/pensieve_web/controllers/memory_html.ex
+++ lib/pensieve_web/controllers/memory_html.ex
@@ -2,4 +2,12 @@ defmodule PensieveWeb.MemoryHTML do
   use PensieveWeb, :html
 
   embed_templates "/memory_html/*"
+
+  def information(assigns) do
+    ~H"""
+    <div class="italic mb-4">
+      There are {length(@memories)} memories in your pensieve
+    </div>
+    """
+  end
 end
